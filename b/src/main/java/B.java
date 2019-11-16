918638d48e80a29cea6f48983e31f4171b65dd58
ts.jia <gl123456>
2019-11-16 08:36:13 +0800

    完善maven文件结构

---
+++ b/src/main/java/B.java
@@ -0,0 +1,7 @@
+/**
+ * @auther: TosinJia
+ * @date: 2019/11/16 08:30
+ * @description:
+ */
+public class B {
+}
---
+++ c/c2/src/main/java/C2.java
@@ -0,0 +1,7 @@
+/**
+ * @auther: TosinJia
+ * @date: 2019/11/16 08:31
+ * @description:
+ */
+public class C2 {
+}
